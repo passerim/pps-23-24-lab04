b71ec56bd248d4c888c9865b575901e8888e6dff
Mattia Passeri <mattia.passeri2@studio.unibo.it>
2024-04-07 15:53:58 +0200

    Completed task 1

--- src/main/scala/tasks/adts/Ex1ComplexNumber.scala
+++ src/main/scala/tasks/adts/Ex1ComplexNumber.scala
@@ -1,6 +1,6 @@
 package tasks.adts
 
-/*  Exercise 1: 
+/*  Exercise 1:
  *  Complete the implementation of ComplexADT trait below, so that it passes
  *  the test in ComplexTest.
  */
@@ -19,12 +19,27 @@ object Ex1ComplexNumbers:
 
   object BasicComplexADT extends ComplexADT:
 
-    // Change assignment below: should probably define a case class and use it?
-    type Complex = Nothing 
-    def complex(re: Double, im: Double): Complex = ???
+    private case class ComplexImpl(re: Double, im: Double)
+
+    opaque type Complex = ComplexImpl
+
+    def complex(re: Double, im: Double): Complex = ComplexImpl(re, im)
+
     extension (complex: Complex)
-      def re(): Double = ???
-      def im(): Double = ???
-      def sum(other: Complex): Complex = ???
-      def subtract(other: Complex): Complex = ???
-      def asString(): String = ???
+      def re(): Double = complex.re
+
+      def im(): Double = complex.im
+
+      def sum(other: Complex): Complex =
+        ComplexImpl(complex.re + other.re, complex.im + other.im)
+
+      def subtract(other: Complex): Complex =
+        ComplexImpl(complex.re - other.re, complex.im - other.im)
+
+      def asString(): String =
+        (complex.re, complex.im) match
+          case (0.0, 0.0)      => "0.0"
+          case (a, 0.0)        => s"$a"
+          case (0.0, b)        => s"${b}i"
+          case (a, b) if b < 0 => s"$a - ${b.abs}i"
+          case (a, b)          => s"$a + ${b}i"
--- src/test/scala/tasks/adts/ComplexNumberTests.scala
+++ src/test/scala/tasks/adts/ComplexNumberTests.scala
@@ -4,9 +4,9 @@ import org.junit.*
 import org.junit.Assert.*
 import tasks.adts.Ex1ComplexNumbers.*
 
-/* Tests should be clear, but note they are expressed independently of the 
+/* Tests should be clear, but note they are expressed independently of the
    specific implementation
-*/
+ */
 
 class ComplexTest:
 
@@ -16,22 +16,22 @@ class ComplexTest:
 
   // From now, everything is independent of specific implementation of Complex
 
-  @Test def testReal() =
+  @Test def testReal(): Unit =
     assertEquals(10, complex(10, 20).re(), 0)
 
-  @Test def testImaginary() =
+  @Test def testImaginary(): Unit =
     assertEquals(20, complex(10, 20).im(), 0)
 
-  @Test def testSum() =
+  @Test def testSum(): Unit =
     assertEquals(complex(11, 22), complex(10, 20) sum complex(1, 2))
 
-  @Test def testSubtract() =
+  @Test def testSubtract(): Unit =
     assertEquals(complex(9, 18), complex(10, 20) subtract complex(1, 2))
 
-  @Test def testAsString() =
+  @Test def testAsString(): Unit =
     assertEquals("10.0 + 5.0i", complex(10.0, 5.0).asString())
 
-  @Test def optionalTestAdvancedAsString() =
+  @Test def optionalTestAdvancedAsString(): Unit =
     assertEquals("0.0", complex(0, 0).asString())
     assertEquals("10.0", complex(10.0, 0).asString())
     assertEquals("10.0 + 5.0i", complex(10.0, 5.0).asString())
