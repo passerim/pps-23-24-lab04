1caf4f4863f4c7d0eaee18a0bf8b32c7b521604f
Mattia Passeri <mattia.passeri2@studio.unibo.it>
2024-04-07 17:17:31 +0200

    Completed task 4

--- src/main/scala/tasks/adts/Ex3Stack.scala
+++ src/main/scala/tasks/adts/Ex3Stack.scala
@@ -24,11 +24,16 @@ object Ex3Stacks:
       def asSequence(): Sequence[A]
 
   object StackImpl extends StackADT:
+
     opaque type Stack[A] = Sequence[A]
+
     def empty[A]: Stack[A] = Nil()
+
     extension [A](stack: Stack[A])
       def push(a: A): Stack[A] = Cons(a, stack)
+
       def pop: Optional[(A, Stack[A])] = stack match
         case Cons(h, t) => Just((h, t))
         case _          => Empty()
+
       def asSequence(): Sequence[A] = stack
--- src/main/scala/tasks/typeclasses/Ex4Summables.scala
+++ src/main/scala/tasks/typeclasses/Ex4Summables.scala
@@ -1,8 +1,9 @@
-package u04lab
-import u03.Sequences.* 
-import Sequence.*
+package tasks.typeclasses
 
-/*  Exercise 4: 
+import u03.Sequences.*
+import u03.Sequences.Sequence.*
+
+/*  Exercise 4:
  *  - Complete the implementation of ad-hoc polymorphic sumAll, using summable.sum and summable.zero
  *  - Write givens also for Summable[Double], Summable[String]
  *  - Uncomment in the main and check if everything works
@@ -12,39 +13,47 @@ object Ex4Summables:
 
   def sumAllInt(seq: Sequence[Int]): Int = seq match
     case Cons(h, t) => h + sumAllInt(t)
-    case _ => 0
+    case _          => 0
 
   trait Summable[A]:
     def sum(a1: A, a2: A): A
     def zero: A
 
-  def sumAll[A: Summable](seq: Sequence[A]) = 
+  def sumAll[A: Summable](seq: Sequence[A]): A =
     val summable = summon[Summable[A]]
-    ???  // complete here
+    seq match
+      case Cons(h, t) => summable.sum(h, sumAll(t))
+      case _          => summable.zero
 
   given Summable[Int] with
     def sum(a1: Int, a2: Int): Int = a1 + a2
+
     def zero: Int = 0
-  
-  // write givens for Summable[Double] and Summable[String]
 
-  @main def trySummables =
-    val si = Cons(10, Cons(20, Cons(30, Nil())))  
-    println:
-      sumAllInt(si) // 60
+  given Summable[Double] with
+    def sum(a1: Double, a2: Double): Double = a1 + a2
 
-    /* uncomment from here   
+    def zero: Double = 0.0
 
-    println:
-      sumAll(si) // 60
+  given Summable[String] with
+    def sum(a1: String, a2: String): String = a1 concat a2
 
-    val sd = Cons(10.0, Cons(20.0, Cons(30.0, Nil())))  
-    println:
-      sumAll(sd) // 60.0
+    def zero: String = ""
 
-    val ss = Cons("10", Cons("20", Cons("30", Nil())))  
-    println:
-      sumAll(ss) // "102030"
+@main def trySummables(): Unit =
+  import Ex4Summables.*
 
-    */  
+  val si = Cons(10, Cons(20, Cons(30, Nil())))
+  println:
+    sumAllInt(si) // 60
 
+  println:
+    sumAll(si) // 60
+
+  val sd = Cons(10.0, Cons(20.0, Cons(30.0, Nil())))
+  println:
+    sumAll(sd) // 60.0
+
+  val ss = Cons("10", Cons("20", Cons("30", Nil())))
+  println:
+    sumAll(ss) // "102030"
